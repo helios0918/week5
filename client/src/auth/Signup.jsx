6496f04a10c11ef8a08f1fab1033791424239fb7
vikramdev772 <vikramdev772@gmail.com>
2026-01-09 15:25:29 +0530

    signup ....

--- client/src/auth/Signup.jsx
+++ client/src/auth/Signup.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 function Signup() {
-    function nameHandler(e){
-        console.log(e.target.value);
-    }
+    let [n,setN]=useState("test");
+    let [E,setE]=useState("test@gmail.com");
+    let [P,setP]=useState("test123");
+  function nameHandler(e) {
+    // console.log(e.target.value);
+      setN(e.target.value);
+  }
   return (
     <>
       <h3>create account</h3>
       <br />
+      <input type="text" placeholder="name" onChange={nameHandler} />
+      <br />
       <input
         type="text"
-        placeholder="name"
-        onChange={nameHandler}
+        placeholder="email"
+        onChange={(e) => {
+        //   console.log(e.target);
+        setE(e.target.value);
+        }}
       />
       <br />
-      <input type="text" placeholder="email" />
-      <br />
-      <input type="text" placeholder="password" />
+      <input type="text" placeholder="password" 
+      onChange={e=>{
+        setP(E.target.value);
+      }}
+      />
       <br />
       <br />
       <button>submit</button>
+      <br />
+      <div>
+        <h4>name : {n}</h4>
+        <h4>email : {E}</h4>
+        <h4>password : {P}</h4>
+
+      </div>
     </>
   );
 }
